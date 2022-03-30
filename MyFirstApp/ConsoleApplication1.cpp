48583158aba52d1fb55c004f4bc3ed8bf2f0156a
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-30 21:16:34 +0500

    new file:   MyFirstApp/ConsoleApplication1.cpp 	new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 \342\204\22616/26 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/26POL.cpp"

---
+++ MyFirstApp/ConsoleApplication1.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>     
+#include <cmath>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+const char oper[4] = { '/', '*','+','-' };
+
+
+
+void A(int pred) {
+    srand(time(NULL));
+
+    float a = rand() % pred;
+    float b = rand() % pred;
+    char operloc = oper[rand() % 4];
+    float ans=0; 
+    switch (operloc)
+    {
+    case '+' : 
+        ans = a + b;
+        break;
+    case '-':
+        ans = a - b;
+        break;
+    case '/':
+         ans = a / b;
+        break;
+    case '*':
+        ans = a * b;
+        break;
+    default:
+        cout << "ERROR!";
+        break;
+    }
+    cout << a << ' ' << operloc << ' ' << b << " = ";
+    float anser; cin >> anser;
+    if (ans == anser) {
+        cout << "Correct!" << endl;
+    }
+    else {
+        cout << "Error" << endl;
+    }
+
+}
+void B(int pred) {
+    srand(time(NULL));
+
+    float a = rand()  % pred;
+    a *= 0.1;
+    float b = rand() % pred;
+    b *= 0.1;
+
+    char operloc = oper[rand() % 4];
+    float ans = 0;
+    switch (operloc)
+    {
+    case '+':
+        ans = a + b;
+        break;
+    case '-':
+        ans = a - b;
+        break;
+    case '/':
+        ans = a / b;
+        break;
+    case '*':
+        ans = a * b;
+        break;
+    default:
+        cout << "ERROR!";
+        break;
+    }
+    cout << a << ' ' << operloc << ' ' << b << " = ";
+    float anser; cin >> anser;
+    if (ans == anser) {
+        cout << "Correct!" << endl;
+    }
+    else {
+        cout << "Error" << endl;
+    }
+
+}
+int main() {   
+    int coin = 0;
+    char exit = 'n';
+    int pred = 0 ;
+    while (exit != 'y') {
+        cout << "integer or float?: ";
+        char valnum; cin >> valnum;
+        if (valnum == 'i') {
+            cout << "predel: ";
+            cin >> pred;
+            A(pred);
+        }
+        else if (valnum == 'f') {
+            cout << "predel ";
+            cin >> pred;
+            B(pred);
+        }
+        else { 
+            cout << "Error!" << endl;
+            continue; 
+        }
+        cout << "Do you want to go ? y/n ";
+        cin >> exit;
+    }
+    return 0;
+}
---
+++ Задания №16/26 задание/26POL.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int n = 0;
+int sm = 0;
+int F(int n)
+{
+    if (n <= 1) {
+        return n + 1;
+    }
+    return n + 1 + n + 5 + F(n - 1) + F(n - 2);
+}
+
+
+
+
+int main()
+{
+
+    while (true) {
+        n += 1;
+        sm = F(n);
+        if (sm > 1000000) {
+            cout << n << ' ' << sm;
+            break;
+        }
+    }       
+}
