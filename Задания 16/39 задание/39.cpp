a3093ae785a660cd51c9b2c78aeb6186672f6813
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-31 21:26:10 +0500

    new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/39 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/39.cpp"

---
+++ Задания 16/39 задание/39.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//int i = 0;
+int F(int n);
+int G(int n);
+int F(int n) {
+    if (n == 1) return 1;
+    if (n > 1) return F(n - 1) - 2 * G(n - 1);
+}
+int G(int n) {
+    if (n == 1) return 1;
+    if (n > 1) return F(n-1) + 2*G(n-1);
+}
+
+int main()
+{
+    cout << G(21);
+}
