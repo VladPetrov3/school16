7ff298cac9aab6d08dca41211cbf13f76d58c028
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-31 21:05:34 +0500

    new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/36 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/36.cpp"

---
+++ Задания 16/36 задание/36.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int i = 0;
+int F(int n, int m)
+{
+    if (m == 0)
+        return n;
+    else
+        return F(m, n % m);
+}
+
+
+
+int main()
+{
+    int c = 0;
+    for (int i = 100; i <= 1000; i++) {
+        bool flag = false;
+        for (int j = 100; j <= 1000; j++)
+        {
+            if (F(i, j) == 30) {
+                c++;
+                break;
+            }
+        }
+    }
+    cout << c;
+}
