186cca4149850229867da6342e27d4f553a34063
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-31 21:37:42 +0500

    new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/50 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/50.cpp"

---
+++ Задания 16/50 задание/50.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//int i = 0;
+int F(int n) {
+    if (n <= 3) {
+        return n;
+    }
+    if (n % 2 == 0 && n > 3) {
+        return n + 3 + F(n - 1);
+    }
+    else if (n % 2 != 0 && n > 3) {
+        return n * n + F(n - 2);
+    }
+}
+
+
+int main()
+{
+    int c = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (F(i) % 7 == 0) {
+            c++;
+        }
+    }
+    cout << c;
+}
