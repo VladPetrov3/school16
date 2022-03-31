985663fb5dab211b0bde7c333184bad5ccfa4bdb
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-31 20:55:02 +0500

    deleted:    "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/20 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/Cpp.cpp" 	new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/20 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/cpp .cpp"

---
+++ Задания 16/20 задание/cpp .cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+/*
+		F(1) = G(1) = 1
+		F(n) = 3·F(n–1) + G(n–1) – n + 5, если n > 1
+		G(n) = F(n–1) + 3·G(n–1) – 3·n, если n > 1
+*/
+using namespace std;
+int F(int n);
+int G(int n);
+int F(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n > 1) {
+		return 3 * F(n - 1) + G(n - 1) - n + 5;
+	}
+}
+int G(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n > 1) {
+		return F(n-1) + 3*G(n-1) - 3*n;
+	}
+}
+int main()
+{
+	cout << F(14) + G(14);
+}
