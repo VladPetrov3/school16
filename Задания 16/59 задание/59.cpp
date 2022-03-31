2aa0c31364a469ac1a5b59aedeb5747829707468
VladPetrov3 <vladpetrovvp3@gmail.com>
2022-03-31 21:57:55 +0500

    new file:   "\320\227\320\260\320\264\320\260\320\275\320\270\321\217 16/59 \320\267\320\260\320\264\320\260\320\275\320\270\320\265/59.cpp"

---
+++ Задания 16/59 задание/59.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//F(n) =  n * n + 5 * n + 4, при n > 30
+//F(n) = F(n + 1) + 3 * F(n + 4), при чётных n <= 30
+//F(n) = 2 * F(n + 2) + F(n + 5), при нечётных n <= 30
+
+int F(int n) {
+    if (n > 30) {
+        return n * n + 5 * n + 4;
+    }
+    if (n % 2 == 0 && n <= 3) {
+        return F(n + 1) + 3 * F(n + 4);
+    }
+    else if (n % 2 != 0 && n <= 3) {
+        return 2 * F(n + 2) + F(n + 5);
+    }
+}
+int sum(int n) {
+    int sum = 0;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int c = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (sum(F(i)) == 27) {
+            c++;
+        }
+    }
+    cout << c;
+}
